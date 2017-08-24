a4410056a8ed8a002706c41b8b106de649426d97
hw_developer <hw_developer@163.com>
2017-08-24 14:14:32 +0800

    change the name

---
+++ application/views/TutorialView.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link href="<?php echo base_url() ?>css/tutorial.css" rel="stylesheet" type="text/css"/>
+    <script src="<?php echo base_url() ?>js/jquery-3.2.1.min.js"></script>
+    <title>后台服务器CI，前端Vue.js和jquery</title>
+
+    <script type="text/javascript">
+
+        function optionAjax() {
+            var url = "<?php echo site_url('Ajax')?>";
+            window.location.href = url;
+        }
+        function formToData() {
+
+            var url = "<?php echo site_url('Form1Controller')?>";
+            window.location.href = url;
+
+        }
+
+    </script>
+
+</head>
+<body>
+
+<h1>后台服务器CI，前端Vue.js和jquery</h1>
+<div id="container">
+    <div id="body">
+        <code onclick="turnTo()">表单数据提交</code>
+        <code onclick="optionAjax()">AJAX</code>
+    </div>
+</div>
+
+
+</body>
+</html>
